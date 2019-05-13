20edc3365bc14797a57af8df2b8f675f479ca46c
Sabrina Settle <ssettle@e1z3r7p6.42.us.org>
2019-05-12 19:52:09 -0700

    supsup

--- conversions/convert_hex.c
+++ conversions/convert_hex.c
@@ -6,11 +6,17 @@
 /*   By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/02 12:33:39 by ssettle           #+#    #+#             */
-/*   Updated: 2019/05/12 11:20:28 by ssettle          ###   ########.fr       */
+/*   Updated: 2019/05/12 19:51:52 by ssettle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+// https://github.com/spitox/ft_printf/blob/master/srcs/print_x.c
+// https://github.com/cbg119/42-ft_printf/blob/master/conversions/convert_x.c
+// https://github.com/spitox/ft_printf/blob/master/padding/print_x_option.c
+// https://github.com/MagicHatJo/-42-ft_printf/blob/master/type/d_x.c
+
 
 static void error_check(t_opts f_opts)
 {
---
+++ mains/main.c
@@ -0,0 +1,34 @@
+#include "libft.h"
+#include "ft_printf.h"
+#include <stdio.h>
+
+//https://github.com/agavrel/42-ft_printf/blob/master/misc/main.c
+
+int	main(void)
+{
+	ft_printf("Characters: %c %c \n", 'a', 65);
+	printf ("Characters: %c %c \n", 'a', 65);
+
+	ft_printf ("Decimals: %d %ld\n", 1977, 650000L);
+	printf ("Decimals: %d %ld\n", 1977, 650000L);
+
+	ft_printf("Preceding with blanks: %10d \n", 1977);
+	printf ("Preceding with blanks: %10d \n", 1977);
+
+	ft_printf("Preceding with zeros: %010d \n", 1977);
+	printf ("Preceding with zeros: %010d \n", 1977);
+
+	ft_printf("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+	printf ("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+
+	ft_printf("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+	printf ("floats: %4.2f %+.0e %E \n", 3.1416, 3.1416, 3.1416);
+
+	ft_printf("Width trick: %*d \n", 5, 10);
+	printf ("Width trick: %*d \n", 5, 10);
+
+	ft_printf ("%s \n", "A string");
+	printf ("%s \n", "A string");
+	return 0;
+}
+
