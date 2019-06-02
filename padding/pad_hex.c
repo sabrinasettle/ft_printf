a6d8423764e24f960d7e184b6324f6709b952ea9
Sabrina Settle <ssettle@e1z2r8p20.42.us.org>
2019-06-02 11:32:05 -0700

    uggghhh VScode you suck

--- padding/pad_hex.c
+++ padding/pad_hex.c
@@ -6,47 +6,31 @@
 /*   By: ssettle <ssettle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 19:49:55 by ssettle           #+#    #+#             */
-/*   Updated: 2019/05/13 15:44:14 by ssettle          ###   ########.fr       */
+/*   Updated: 2019/06/02 11:24:04 by ssettle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
-//https://github.com/spitox/ft_printf/blob/master/srcs/print_x.c
 
-static int	split(int n, int tmp, int len, t_opts f_opts)
+char	*padding_p(char *s, t_opts opts)
 {
-	t_flags flags;
+	char	*str;
 
-	while (tmp > 0 && !flags.minus)
-	{
-		if(flags.zero && !f_opts.precision)
-			pf_putchar('0');
-		else
-			pf_putchar(' ');
-		tmp--;
-		len++;
-	}
-	if( == -1)
-	{
-		len += 2;
-		write(1, "0x", 2);
-	}
+	if (!(str = (char *)pf_strnew(opts.width)))
+		return ();
+	pf_memset(str, ' ', opts.width);
+	if (opts.flags.minus)
+		pf_strncpy(str, s, 14);
+	else
+		pf_strncpy(&str[opts.width - 14], s, 14);
+	free(s);
+	return (str);
 }
 
-static int	split_twice(int n, int *tmp, int len, t_opts f_opts)
+char	*padding_lh(char *s, int len, t_opts opts)
 {
-	if(n)
-		tmp -= 2;
-	if()
-	{
-		write(1, "0x", 2);
-		len += 2;
-	}
-	return (len);
-}
+	char	*str;
 
-static int	split_three(int n, int i, t_opts f_opts)
-{
-	
+	if (!(str = (char *)))
 
 }
