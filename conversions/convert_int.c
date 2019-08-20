e45866ac87500e5245994a8a97c5d2601978f349
Settle <ssettle93@gmail.com>
2019-08-20 03:40:15 -0700

    ok so just print a number and then handle negatives ok??

--- conversions/convert_int.c
+++ conversions/convert_int.c
@@ -34,53 +34,94 @@
 // j = intmax_t 106
 // z = size_t 122
 
-int     content_sizing(t_opts options, va_list ap) //instead of options-> int?
-{
-    int64_t c;
+// int     content_sizing(t_opts options, va_list ap) //instead of options-> int?
+// {
+//     int64_t c;
     
-	if (options.content_size == 0)
-		c = va_arg(ap, int);
-	else if (options.content_size == 108) //does not work
-		c = va_arg(ap, long int);
-    else if (options.content_size == 104) //does not work
-		c = va_arg(ap, short int);
-    else if (options.content_size == 216) //does not work
-		c = va_arg(ap, long long int);
-	else if (options.content_size == 208) //does not work
-		c = va_arg(ap, char); //same as signed char
-	else if (options.content_size == 106) //does not work
-		c = va_arg(ap, intmax_t);
-	else if (options.content_size == 122) //does not work
-		c = va_arg(ap, size_t);
-    return(c);
-}
+// 	if (options.content_size == 0)
+// 		c = va_arg(ap, int);
+// 	else if (options.content_size == 108) //does not work
+// 		c = va_arg(ap, long int);
+//     else if (options.content_size == 104) //does not work
+// 		c = va_arg(ap, short int);
+//     else if (options.content_size == 216) //does not work
+// 		c = va_arg(ap, long long int);
+// 	else if (options.content_size == 208) //does not work
+// 		c = va_arg(ap, char); //same as signed char
+// 	else if (options.content_size == 106) //does not work
+// 		c = va_arg(ap, intmax_t);
+// 	else if (options.content_size == 122) //does not work
+// 		c = va_arg(ap, size_t);
+//     return(c);
+// }
 
+// char	*str_prec(t_opts options, char *str)
+// {
+// 	int32_t		len;
+// 	char 		*new_str;
 
-void	put_sign(t_opts options, )
+// 	len = options.precision;
+// 	(void)options.flags.minus;
+// 	new_str = pf_strsub(str, 0, len);
+// 	return(new_str);
+// 	// free(new_str);
+// }
+
+// char		*padding_str(t_opts options, char *str)
+// {
+// 	int wd_len;
+// 	int len;
+// 	int new_len;
+// 	char *new_str;
+
+// 	new_len = 0;
+// 	len = pf_strlen(str);
+// 	wd_len = options.width_field;
+// 	new_str = pf_strdup(str);
+// 	pf_memset(new_str, ' ', wd_len);
+// 	new_str[wd_len] = '\0';
+// 	if (options.flags.minus >= 1)
+// 		pf_strncpy(new_str, str, len);
+// 	else
+// 		new_len = wd_len - len;
+// 		pf_strncpy(&new_str[new_len], str, len);
+// 	return(new_str);
+// 	// free(str);
+// }
+
+// padding with zeros needs to happen as well
+
+int     convert_int(t_opts options, va_list ap)
 {
+	char		*str;
+	int			str_len;
+	int64_t 	n;
+	int			neg;
 
-}
-
-int     content_int(t_opts options, va_list ap)
-{
-	char	*str;
-	int		str_len;
-	int 	n;
-	int		neg;
-
-	n = content_sizing(options, ap);
-	neg = (n < 0) ? 1 : 0;
+	// n = content_sizing(options, ap);
+	neg = (n < 0) ? 1 : 0; //could be macro???
 	str = pf_itoa((char *)va_arg(options, int); //abs?
 	if (neg)
 	{
 		pf_putchar('-');
+		ft_printf("I returned negative!");
 	}
-		// str =padd_string_left(string, &length, special, str);
-	if ()
-		// str = padd_string_right(string, length, special, str);
-	if ()
-		
-    pf_putstr(str);
+	// if (options.width_field > len)
+	// {
+	// 	new_str = padding_str(options, str);
+	// 	if (options.precision <= len && options.precision)
+	// 		new_str = padding_str(options, str_prec(options, str));
+	// 	pf_putstr(new_str);
+	// }
+	// else if (options.precision <= len && options.precision)
+	// {
+	// 	new_str = str_prec(options, str);
+	// 	pf_putstr(new_str);
+	// 	free(new_str);
+	// 	// free(str);
+	// }
+	// else 
+	pf_putstr(str);
 	str_len = ft_strlen(str);
 	// free(str);
 	return(str_len);
